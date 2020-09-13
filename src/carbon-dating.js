a20c1de6730d468b694ada41041f62391f841255
Ivan <followhappyq@gmail.com>
2020-09-13 18:56:15 +0300

    feat: complete cardbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,13 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require("../extensions/custom-error")
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15
+const HALF_LIFE_PERIOD = 5730
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function dateSample(sampleActivity) {
+  let sample = parseFloat(sampleActivity)
+  if (typeof sampleActivity !== "string" || sample > 15 || sample <= 0 || isNaN(sample)) {
+    return false
+  }
+
+  return Math.ceil((Math.log(MODERN_ACTIVITY / sample) * HALF_LIFE_PERIOD) / 0.693)
+}
