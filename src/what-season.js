93002e2e67d9209cea26f60b2590fc78454d0be3
Ivan <followhappyq@gmail.com>
2020-09-13 18:35:36 +0300

    feat: complete tasks what-season, count-cat,dream-team

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,22 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require("../extensions/custom-error")
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function getSeason(date) {
+  if (!date) return "Unable to determine the time of year!"
+
+  if (date.hasOwnProperty("toString")) {
+    throw new Error()
+  }
+  const month = date.getMonth()
+  if (month === 0 || month === 1 || month == 11) {
+    return "winter"
+  }
+  if (month > 1 && month < 5) {
+    return "spring"
+  }
+  if (month > 4 && month < 8) {
+    return "summer"
+  }
+  if (month > 7 && month < 11) {
+    return "autumn"
+  }
+}
